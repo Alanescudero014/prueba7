eef44c24b53a0ff011193aa1becc0687e29217a4
Alanescudero014 <252118003ajescuderoa@gmail.com>
2023-02-27 15:02:04 -0600

    20 res

--- form.php
+++ form.php
@@ -69,6 +69,76 @@
 <input type="radio" id="d10" name="res10" value="0" checked="true" style="opacity: 0;">
 <script src="respuestas.js"></script>
 <br><br>
+<label for="gender">Pregunta 11: </label>
+<input type="radio" id="a11" name="res11" value="1" onclick="ar11()">Rojo
+<input type="radio" id="b11" name="res11" value="2" onclick="br11()">Azul
+<input type="radio" id="c11" name="res11" value="3" onclick="cr11()">Amarillo
+<input type="radio" id="d11" name="res11" value="0" checked="true" style="opacity: 0;">
+<script src="respuestas.js"></script>
+<br><br>
+<label for="gender">Pregunta 12: </label>
+<input type="radio" id="a12" name="res12" value="1" onclick="ar12()">Rojo
+<input type="radio" id="b12" name="res12" value="2" onclick="br12()">Azul
+<input type="radio" id="c12" name="res12" value="3" onclick="cr12()">Amarillo
+<input type="radio" id="d12" name="res12" value="0" checked="true" style="opacity: 0;">
+<script src="respuestas.js"></script>
+<br><br>
+<label for="gender">Pregunta 13: </label>
+<input type="radio" id="a13" name="res13" value="1" onclick="ar13()">Rojo
+<input type="radio" id="b13" name="res13" value="2" onclick="br13()">Azul
+<input type="radio" id="c13" name="res13" value="3" onclick="cr13()">Amarillo
+<input type="radio" id="d13" name="res13" value="0" checked="true" style="opacity: 0;">
+<script src="respuestas.js"></script>
+<br><br>
+<label for="gender">Pregunta 14: </label>
+<input type="radio" id="a14" name="res14" value="1" onclick="ar14()">Rojo
+<input type="radio" id="b14" name="res14" value="2" onclick="br14()">Azul
+<input type="radio" id="c14" name="res14" value="3" onclick="cr14()">Amarillo
+<input type="radio" id="d14" name="res14" value="0" checked="true" style="opacity: 0;">
+<script src="respuestas.js"></script>
+<br><br>
+<label for="gender">Pregunta 15: </label>
+<input type="radio" id="a15" name="res15" value="1" onclick="ar15()">Rojo
+<input type="radio" id="b15" name="res15" value="2" onclick="br15()">Azul
+<input type="radio" id="c15" name="res15" value="3" onclick="cr15()">Amarillo
+<input type="radio" id="d15" name="res15" value="0" checked="true" style="opacity: 0;">
+<script src="respuestas.js"></script>
+<br><br>
+<label for="gender">Pregunta 16: </label>
+<input type="radio" id="a16" name="res16" value="1" onclick="ar16()">Rojo
+<input type="radio" id="b16" name="res16" value="2" onclick="br16()">Azul
+<input type="radio" id="c16" name="res16" value="3" onclick="cr16()">Amarillo
+<input type="radio" id="d16" name="res16" value="0" checked="true" style="opacity: 0;">
+<script src="respuestas.js"></script>
+<br><br>
+<label for="gender">Pregunta 17: </label>
+<input type="radio" id="a17" name="res17" value="1" onclick="ar17()">Rojo
+<input type="radio" id="b17" name="res17" value="2" onclick="br17()">Azul
+<input type="radio" id="c17" name="res17" value="3" onclick="cr17()">Amarillo
+<input type="radio" id="d17" name="res17" value="0" checked="true" style="opacity: 0;">
+<script src="respuestas.js"></script>
+<br><br>
+<label for="gender">Pregunta 18: </label>
+<input type="radio" id="a18" name="res18" value="1" onclick="ar18()">Rojo
+<input type="radio" id="b18" name="res18" value="2" onclick="br18()">Azul
+<input type="radio" id="c18" name="res18" value="3" onclick="cr18()">Amarillo
+<input type="radio" id="d18" name="res18" value="0" checked="true" style="opacity: 0;">
+<script src="respuestas.js"></script>
+<br><br>
+<label for="gender">Pregunta 19: </label>
+<input type="radio" id="a19" name="res19" value="1" onclick="ar19()">Rojo
+<input type="radio" id="b19" name="res19" value="2" onclick="br19()">Azul
+<input type="radio" id="c19" name="res19" value="3" onclick="cr19()">Amarillo
+<input type="radio" id="d19" name="res19" value="0" checked="true" style="opacity: 0;">
+<script src="respuestas.js"></script>
+<br><br>
+<label for="gender">Pregunta 20: </label>
+<input type="radio" id="a20" name="res20" value="1" onclick="ar20()">Rojo
+<input type="radio" id="b20" name="res20" value="2" onclick="br20()">Azul
+<input type="radio" id="c20" name="res20" value="3" onclick="cr20()">Amarillo
+<input type="radio" id="d20" name="res20" value="0" checked="true" style="opacity: 0;">
+<script src="respuestas.js"></script>
+<br><br>
 <button onclick="guardar()">BORRAR</button>
 </body>
 </html>
--- respuestas.js
+++ respuestas.js
@@ -408,6 +408,420 @@ function cr10(){
     console.log(res10);
     localStorage.res10 = 3;
 }
+
+
+/*******************************  PREGUNTA 11  **************************************/
+if(localStorage.res11 == null){
+    res11 = 0;
+}else{
+    res11 = localStorage.res11;
+}
+if(localStorage.res11 == 1){
+    function checkByDefault(){
+        document.getElementById("a11").checked = true;      
+    }
+
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res11 == 2){
+    function checkByDefault(){
+        document.getElementById("b11").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res11 == 3){
+    function checkByDefault(){
+        document.getElementById("c11").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}
+
+// DAR CLIC 
+
+function ar11(){
+    res11 = document.querySelector('input[name=res11]:checked').value;
+    console.log(res11);
+    localStorage.res11 = 1;
+}
+function br11(){
+    res11 = document.querySelector('input[name=res11]:checked').value;
+    console.log(res11);
+    localStorage.res11 = 2;
+}
+function cr11(){
+    res11 = document.querySelector('input[name=res11]:checked').value;
+    console.log(res11);
+    localStorage.res11 = 3;
+}
+
+/*******************************  PREGUNTA 12  **************************************/
+if(localStorage.res12 == null){
+    res12 = 0;
+}else{
+    res12 = localStorage.res12;
+}
+if(localStorage.res12 == 1){
+    function checkByDefault(){
+        document.getElementById("a12").checked = true;      
+    }
+
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res12 == 2){
+    function checkByDefault(){
+        document.getElementById("b12").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res12 == 3){
+    function checkByDefault(){
+        document.getElementById("c12").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}
+
+// DAR CLIC 
+
+function ar12(){
+    res12 = document.querySelector('input[name=res12]:checked').value;
+    console.log(res12);
+    localStorage.res12 = 1;
+}
+function br12(){
+    res12 = document.querySelector('input[name=res12]:checked').value;
+    console.log(res12);
+    localStorage.res12 = 2;
+}
+function cr12(){
+    res12 = document.querySelector('input[name=res12]:checked').value;
+    console.log(res12);
+    localStorage.res12 = 3;
+}
+
+/*******************************  PREGUNTA 13  **************************************/
+if(localStorage.res13 == null){
+    res13 = 0;
+}else{
+    res13 = localStorage.res13;
+}
+if(localStorage.res13 == 1){
+    function checkByDefault(){
+        document.getElementById("a13").checked = true;      
+    }
+
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res13 == 2){
+    function checkByDefault(){
+        document.getElementById("b13").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res13 == 3){
+    function checkByDefault(){
+        document.getElementById("c13").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}
+
+// DAR CLIC 
+
+function ar13(){
+    res13 = document.querySelector('input[name=res13]:checked').value;
+    console.log(res13);
+    localStorage.res13 = 1;
+}
+function br13(){
+    res13 = document.querySelector('input[name=res13]:checked').value;
+    console.log(res13);
+    localStorage.res13 = 2;
+}
+function cr13(){
+    res13 = document.querySelector('input[name=res13]:checked').value;
+    console.log(res13);
+    localStorage.res13 = 3;
+}
+/*******************************  PREGUNTA 14  **************************************/
+if(localStorage.res14 == null){
+    res14 = 0;
+}else{
+    res14 = localStorage.res14;
+}
+if(localStorage.res14 == 1){
+    function checkByDefault(){
+        document.getElementById("a14").checked = true;      
+    }
+
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res14 == 2){
+    function checkByDefault(){
+        document.getElementById("b14").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res14 == 3){
+    function checkByDefault(){
+        document.getElementById("c14").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}
+
+// DAR CLIC 
+
+function ar14(){
+    res14 = document.querySelector('input[name=res14]:checked').value;
+    console.log(res14);
+    localStorage.res14 = 1;
+}
+function br14(){
+    res14 = document.querySelector('input[name=res14]:checked').value;
+    console.log(res14);
+    localStorage.res14 = 2;
+}
+function cr14(){
+    res14 = document.querySelector('input[name=res14]:checked').value;
+    console.log(res14);
+    localStorage.res14 = 3;
+}
+/*******************************  PREGUNTA 15  **************************************/
+if(localStorage.res15 == null){
+    res15 = 0;
+}else{
+    res15 = localStorage.res15;
+}
+if(localStorage.res15 == 1){
+    function checkByDefault(){
+        document.getElementById("a15").checked = true;      
+    }
+
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res15 == 2){
+    function checkByDefault(){
+        document.getElementById("b15").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res15 == 3){
+    function checkByDefault(){
+        document.getElementById("c15").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}
+
+// DAR CLIC 
+
+function ar15(){
+    res15 = document.querySelector('input[name=res15]:checked').value;
+    console.log(res15);
+    localStorage.res15 = 1;
+}
+function br15(){
+    res15 = document.querySelector('input[name=res15]:checked').value;
+    console.log(res15);
+    localStorage.res15 = 2;
+}
+function cr15(){
+    res15 = document.querySelector('input[name=res15]:checked').value;
+    console.log(res15);
+    localStorage.res15 = 3;
+}
+/*******************************  PREGUNTA 16  **************************************/
+if(localStorage.res16 == null){
+    res16 = 0;
+}else{
+    res16 = localStorage.res16;
+}
+if(localStorage.res16 == 1){
+    function checkByDefault(){
+        document.getElementById("a16").checked = true;      
+    }
+
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res16 == 2){
+    function checkByDefault(){
+        document.getElementById("b16").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res16 == 3){
+    function checkByDefault(){
+        document.getElementById("c16").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}
+
+// DAR CLIC 
+
+function ar16(){
+    res16 = document.querySelector('input[name=res16]:checked').value;
+    console.log(res16);
+    localStorage.res16 = 1;
+}
+function br16(){
+    res16 = document.querySelector('input[name=res16]:checked').value;
+    console.log(res16);
+    localStorage.res16 = 2;
+}
+function cr16(){
+    res16 = document.querySelector('input[name=res16]:checked').value;
+    console.log(res16);
+    localStorage.res16 = 3;
+}
+/*******************************  PREGUNTA 17  **************************************/
+if(localStorage.res17 == null){
+    res17 = 0;
+}else{
+    res17 = localStorage.res17;
+}
+if(localStorage.res17 == 1){
+    function checkByDefault(){
+        document.getElementById("a17").checked = true;      
+    }
+
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res17 == 2){
+    function checkByDefault(){
+        document.getElementById("b17").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res17 == 3){
+    function checkByDefault(){
+        document.getElementById("c17").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}
+
+// DAR CLIC 
+
+function ar17(){
+    res17 = document.querySelector('input[name=res17]:checked').value;
+    console.log(res17);
+    localStorage.res17 = 1;
+}
+function br17(){
+    res17 = document.querySelector('input[name=res17]:checked').value;
+    console.log(res17);
+    localStorage.res17 = 2;
+}
+function cr17(){
+    res17 = document.querySelector('input[name=res17]:checked').value;
+    console.log(res17);
+    localStorage.res17 = 3;
+}
+/*******************************  PREGUNTA 18  **************************************/
+if(localStorage.res18 == null){
+    res18 = 0;
+}else{
+    res18 = localStorage.res18;
+}
+if(localStorage.res18 == 1){
+    function checkByDefault(){
+        document.getElementById("a18").checked = true;      
+    }
+
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res18 == 2){
+    function checkByDefault(){
+        document.getElementById("b18").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res18 == 3){
+    function checkByDefault(){
+        document.getElementById("c18").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}
+
+// DAR CLIC 
+
+function ar18(){
+    res18 = document.querySelector('input[name=res18]:checked').value;
+    console.log(res18);
+    localStorage.res18 = 1;
+}
+function br18(){
+    res18 = document.querySelector('input[name=res18]:checked').value;
+    console.log(res18);
+    localStorage.res18 = 2;
+}
+function cr18(){
+    res18 = document.querySelector('input[name=res18]:checked').value;
+    console.log(res18);
+    localStorage.res18 = 3;
+}
+/*******************************  PREGUNTA 19  **************************************/
+if(localStorage.res19 == null){
+    res19 = 0;
+}else{
+    res19 = localStorage.res19;
+}
+if(localStorage.res19 == 1){
+    function checkByDefault(){
+        document.getElementById("a19").checked = true;      
+    }
+
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res19 == 2){
+    function checkByDefault(){
+        document.getElementById("b19").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res19 == 3){
+    function checkByDefault(){
+        document.getElementById("c19").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}
+
+// DAR CLIC 
+
+function ar19(){
+    res19 = document.querySelector('input[name=res19]:checked').value;
+    console.log(res19);
+    localStorage.res19 = 1;
+}
+function br19(){
+    res19 = document.querySelector('input[name=res19]:checked').value;
+    console.log(res19);
+    localStorage.res19 = 2;
+}
+function cr19(){
+    res19 = document.querySelector('input[name=res19]:checked').value;
+    console.log(res19);
+    localStorage.res19 = 3;
+}
+/*******************************  PREGUNTA 20  **************************************/
+if(localStorage.res20 == null){
+    res20 = 0;
+}else{
+    res20 = localStorage.res20;
+}
+if(localStorage.res20 == 1){
+    function checkByDefault(){
+        document.getElementById("a20").checked = true;      
+    }
+
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res20 == 2){
+    function checkByDefault(){
+        document.getElementById("b20").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}else if(localStorage.res20 == 3){
+    function checkByDefault(){
+        document.getElementById("c20").checked = true;     
+    }
+    window.addEventListener('DOMContentLoaded', checkByDefault);
+}
+
+// DAR CLIC 
+
+function ar20(){
+    res20 = document.querySelector('input[name=res20]:checked').value;
+    console.log(res20);
+    localStorage.res20 = 1;
+}
+function br20(){
+    res20 = document.querySelector('input[name=res20]:checked').value;
+    console.log(res20);
+    localStorage.res20 = 2;
+}
+function cr20(){
+    res20 = document.querySelector('input[name=res20]:checked').value;
+    console.log(res20);
+    localStorage.res20 = 3;
+}
 //BOTÓN PARA GUARDAR
 
 function guardar(){
@@ -421,5 +835,15 @@ function guardar(){
     delete localStorage.res8;
     delete localStorage.res9;
     delete localStorage.res10;
+    delete localStorage.res11;
+    delete localStorage.res12;
+    delete localStorage.res13;
+    delete localStorage.res14;
+    delete localStorage.res15;
+    delete localStorage.res16;
+    delete localStorage.res17;
+    delete localStorage.res18;
+    delete localStorage.res19;
+    delete localStorage.res20;
     window.addEventListener('DOMContentLoaded', checkByDefault);
 }
