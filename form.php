4835c5760ef8764fb3b106bb626759a4145d1c7d
Alanescudero014 <252118003ajescuderoa@gmail.com>
2023-02-27 14:21:45 -0600

    opacity

--- form.php
+++ form.php
@@ -12,61 +12,61 @@
 <input type="radio" id="a1" name="res1" value="1" onclick="ar1()">Rojo
 <input type="radio" id="b1" name="res1" value="2"  onclick="br1()">Azul
 <input type="radio" id="c1" name="res1" value="3"  onclick="cr1()">Amarillo
-<input type="radio" id="d1" name="res1" value="0" checked="true">Verde
+<input type="radio" id="d1" name="res1" value="0" checked="true" style="opacity: 0;">
 <br><br>
 <label for="gender">Pregunta 2: </label>
 <input type="radio" id="a2" name="res2" value="1" onclick="ar2()">Rojo
 <input type="radio" id="b2" name="res2" value="2"  onclick="br2()">Azul
 <input type="radio" id="c2" name="res2" value="3"  onclick="cr2()">Amarillo
-<input type="radio" id="d2" name="res2" value="0" checked="true">Verde
+<input type="radio" id="d2" name="res2" value="0" checked="true" style="opacity: 0;">
 <br><br>
 <label for="gender">Pregunta 3: </label>
 <input type="radio" id="a3" name="res3" value="1" onclick="ar3()">Rojo
 <input type="radio" id="b3" name="res3" value="2"  onclick="br3()">Azul
 <input type="radio" id="c3" name="res3" value="3"  onclick="cr3()">Amarillo
-<input type="radio" id="d3" name="res3" value="0" checked="true">Verde
+<input type="radio" id="d3" name="res3" value="0" checked="true" style="opacity: 0;">
 <br><br>
 <label for="gender">Pregunta 4: </label>
 <input type="radio" id="a4" name="res4" value="1" onclick="ar4()">Rojo
 <input type="radio" id="b4" name="res4" value="2"  onclick="br4()">Azul
 <input type="radio" id="c4" name="res4" value="3"  onclick="cr4()">Amarillo
-<input type="radio" id="d4" name="res4" value="0" checked="true">Verde
+<input type="radio" id="d4" name="res4" value="0" checked="true" style="opacity: 0;">
 <br><br>
 <label for="gender">Pregunta 5: </label>
 <input type="radio" id="a5" name="res5" value="1" onclick="ar5()">Rojo
 <input type="radio" id="b5" name="res5" value="2"  onclick="br5()">Azul
 <input type="radio" id="c5" name="res5" value="3"  onclick="cr5()">Amarillo
-<input type="radio" id="d5" name="res5" value="0" checked="true">Verde
+<input type="radio" id="d5" name="res5" value="0" checked="true" style="opacity: 0;">
 <br><br>
 <label for="gender">Pregunta 6: </label>
 <input type="radio" id="a6" name="res6" value="1" onclick="ar6()">Rojo
 <input type="radio" id="b6" name="res6" value="2"  onclick="br6()">Azul
 <input type="radio" id="c6" name="res6" value="3"  onclick="cr6()">Amarillo
-<input type="radio" id="d6" name="res6" value="0" checked="true">Verde
+<input type="radio" id="d6" name="res6" value="0" checked="true" style="opacity: 0;">
 <br><br>
 <label for="gender">Pregunta 7: </label>
 <input type="radio" id="a7" name="res7" value="1" onclick="ar7()">Rojo
 <input type="radio" id="b7" name="res7" value="2"  onclick="br7()">Azul
 <input type="radio" id="c7" name="res7" value="3"  onclick="cr7()">Amarillo
-<input type="radio" id="d7" name="res7" value="0" checked="true">Verde
+<input type="radio" id="d7" name="res7" value="0" checked="true" style="opacity: 0;">
 <br><br>
 <label for="gender">Pregunta 8: </label>
 <input type="radio" id="a8" name="res8" value="1" onclick="ar8()">Rojo
 <input type="radio" id="b8" name="res8" value="2"  onclick="br8()">Azul
 <input type="radio" id="c8" name="res8" value="3"  onclick="cr8()">Amarillo
-<input type="radio" id="d8" name="res8" value="0" checked="true">Verde
+<input type="radio" id="d8" name="res8" value="0" checked="true" style="opacity: 0;">
 <br><br>
 <label for="gender">Pregunta 9: </label>
 <input type="radio" id="a9" name="res9" value="1" onclick="ar9()">Rojo
 <input type="radio" id="b9" name="res9" value="2" onclick="br9()">Azul
 <input type="radio" id="c9" name="res9" value="3" onclick="cr9()">Amarillo
-<input type="radio" id="d9" name="res9" value="0" checked="true">Verde
+<input type="radio" id="d9" name="res9" value="0" checked="true" style="opacity: 0;">
 <br><br>
 <label for="gender">Pregunta 10: </label>
 <input type="radio" id="a10" name="res10" value="1" onclick="ar10()">Rojo
 <input type="radio" id="b10" name="res10" value="2" onclick="br10()">Azul
 <input type="radio" id="c10" name="res10" value="3" onclick="cr10()">Amarillo
-<input type="radio" id="d10" name="res10" value="0" checked="true">Verde
+<input type="radio" id="d10" name="res10" value="0" checked="true" style="opacity: 0;">
 <script src="respuestas.js"></script>
 <br><br>
 <button onclick="guardar()">BORRAR</button>
