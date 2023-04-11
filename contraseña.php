1600fc4afbca7f99f23f81e69be5c113ef70f856
Alanescudero014 <252118003ajescuderoa@gmail.com>
2023-04-11 14:54:18 -0600

    11/04/2023

---
+++ contraseña.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>INICIO DE SESIÓN</title>
+  <link rel="stylesheet" type="text/css" href="css/estilos1.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" rel="stylesheet"> 
+  <link rel="shortcut icon" href="img/AIFA.png">
+</head>
+<style>
+      footer{
+	background-color: #111F2D;
+	height: 50px;
+	width: 100%;
+	position: absolute;
+
+	text-align:center;
+  }
+</style>
+<body><!--
+    <header class="navbar sticky-top flex-md-nowrap p-1 text-white m" style="background-color: #002b49">
+        <a class="navbar-brand p-2">
+            <img src="img/logo.svg" height="60rem"></a>
+        <div class="navbar-nav">
+        </div>
+        <a style="color: white;" class="btn btn-outline-primary" href="../index.php"><i class="fa-solid fa-rotate-left" style="color: #ffffff;"></i> ATRÁS</a>
+  </header>-->
+  <div class="login-root">
+    <div class="box-root flex-flex flex-direction--column" style="min-height: 100vh;flex-grow: 1;">
+      <div class="loginbackground box-background--white padding-top--64">
+        <div class="loginbackground-gridContainer">
+          <div class="box-root flex-flex" style="grid-area: top / start / 8 / end;">
+            <div class="box-root" style="background-image: linear-gradient(white 0%, rgb(247, 250, 252) 33%); flex-grow: 1;">
+            </div>
+          </div>
+          <div class="box-root flex-flex" style="grid-area: 4 / 2 / auto / 5;">
+            <div class="box-root box-divider--light-all-2 animationLeftRight tans3s" style="flex-grow: 1;"></div>
+          </div>
+          <div class="box-root flex-flex" style="grid-area: 6 / start / auto / 2;">
+            <div class="box-root box-background--blue800" style="flex-grow: 1;"></div>
+          </div>
+          <div class="box-root flex-flex" style="grid-area: 7 / start / auto / 4;">
+            <div class="box-root box-background--blue animationLeftRight" style="flex-grow: 1;"></div>
+          </div>
+          <div class="box-root flex-flex" style="grid-area: 8 / 4 / auto / 6;">
+            <div class="box-root box-background--gray100 animationLeftRight tans3s" style="flex-grow: 1;"></div>
+          </div>
+          <div class="box-root flex-flex" style="grid-area: 2 / 15 / auto / end;">
+            <div class="box-root box-background--cyan200 animationRightLeft tans4s" style="flex-grow: 1;"></div>
+          </div>
+          <div class="box-root flex-flex" style="grid-area: 3 / 14 / auto / end;">
+            <div class="box-root box-background--blue animationRightLeft" style="flex-grow: 1;"></div>
+          </div>
+          <div class="box-root flex-flex" style="grid-area: 4 / 17 / auto / 20;">
+            <div class="box-root box-background--gray100 animationRightLeft tans4s" style="flex-grow: 1;"></div>
+          </div>
+          <div class="box-root flex-flex" style="grid-area: 5 / 14 / auto / 17;">
+            <div class="box-root box-divider--light-all-2 animationRightLeft tans3s" style="flex-grow: 1;"></div>
+          </div>
+        </div>
+      </div><!--
+      <div class="box-root padding-top--24 flex-flex flex-direction--column" style="flex-grow: 1; z-index: 9;">
+        <div class="box-root padding-top--48 padding-bottom--24 flex-flex flex-justifyContent--center">
+          <h1>AIFA</h1>
+        </div>
+        <div class="formbg-outer">
+          <div class="formbg">
+            <div class="formbg-inner padding-horizontal--48">
+              <span class="padding-bottom--15">Recuperar Contraseña</span>
+              <form id="stripe-login" method="post" action="modelo/entrar.php">
+                <div class="field padding-bottom--24">
+                  <label for="email">Ingresa tu correo para enviar tu contraseña por email:</label>
+                  <input type="email" name="ingresoUsuario" required> Ingreso del correo del usuario
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="footer-link padding-top--24"> 
+            <div class="listing padding-top--24 padding-bottom--24 flex-flex center-center">
+              <span><a href="#">© 2023 Aeropuerto Internacional Felipe Ángeles.</a></span>
+              <span><a href="#">Contacto</a></span>
+              <span><a href="#">Privacidad y Terminos</a></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+<footer>
+	<p style="color: white;">Copyright © 2023 Aeropuerto Internacional Felipe Ángeles. </p>
+</footer> -->
+
+</html>
--- hora.php
+++ hora.php
@@ -1,17 +1,43 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    section {
+    background-color: #111F2D;
+    color: white;
+    width: 230px;
+    height: 25px;
+    border-radius: 40px;
+    text-align: center;
+}
+</style>
+<body>
+    
 <script>
-    var today = new Date();
-    var entrada = new Date('03/07/2023 09:21:00')
-    console.log(today);
-    console.log(entrada);
 
-    totale = (today - entrada)/1000;
-    x = Math.round(totale);
-    console.log(x);
-
-    if(x >= 350){
-        alert("Acabo el tiempo");
-    }else{
-
+function tiempo() {
+    
+    fecha.setMinutes(fecha.getMinutes() + 5);
+    console.log(fecha);
     }
+    setInterval('tiempo()',1000);
+
+
 </script>
+    <section>
+                <p>
+                    <span id="input"></span>
+                </p>
+
+
+            </section>
+
+</body>
+</html>
+
+
